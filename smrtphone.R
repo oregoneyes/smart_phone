baefc319e4f6310509c23c9f54e7f02a612d4a5f
oregoneyes <russellsmailbox@yahoo.com>
2021-08-23 07:26:27 -0700

    new commit with completed project.

--- smrtphone.R
+++ smrtphone.R
@@ -78,9 +78,44 @@ red_sub_test <- sub_test %>% select(activity, subject, contains(c('mean','std'))
 red_sub_test$activity <- red_sub_test$activity$X1
 red_sub_test$subject <- red_sub_test$subject$X1
 
-#group_by activity and subject and summarize means
+df_by_subject <- split(red_sub_test, red_sub_test$subject) 
 
-#means_df <- red_sub_test %>% group_by(activity, subject)
+# initialize empty data frames for loop processing
 
-#means_df <- colMeans(means_df[3:88]) %>% summarise(activity, subject)   
+# act_means - holds column means for 1 activity and 1 subject.
+act_means <- as_tibble(NULL)
+
+# sub_means <- vector adding subject identity to the act_means vector
+sub_means <- as_tibble(NULL)
+
+# mean_df - holds new data frame with each subjects processed data grouped by activity
+means_df <- as_tibble(NULL)
+
+# loop to break out each subject's data for processing
+
+for(sub in df_by_subject) {
     
+    # separate all variables by activity into a list
+    df_by_activity_list <- split(sub, sub$activity)
+    
+    # loop to analyze each activity and get the means for all variables. Store in act_mean
+    for(act in df_by_activity_list) {
+        act_mean <- colMeans(act[3:88])
+        
+        # add back activity variable to act_mean
+        act_mean <- append(act[1,1], act_mean)
+        
+        # accumulate act_mean data in act_means
+        act_means <- bind_rows(act_means,act_mean)
+        
+    }
+    
+    # add subject variable to act_means and store in sub_means
+    sub_means <- append(sub[1,2], act_means)
+    
+    # store all sub_means rows into means_df
+    means_df <- bind_rows(means_df, sub_means)
+    
+    
+    
+}
